f281ff8a8129bb8f62301532203d2dbb312a8c76
Kmikmilo <lokomilo1@gmail.com>
2024-05-29 13:01:12 -0400

    first commit

--- dist/controllers/user.controller.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const getUsers = (req, res) => {
-};
-//# sourceMappingURL=user.controller.js.map
--- dist/routes/services_category.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const validateJWT_1 = require("../middleware/validateJWT");
-const servicesCategory_1 = require("../controllers/servicesCategory");
-const router = (0, express_1.Router)();
-router.get('/', validateJWT_1.validateJWT, servicesCategory_1.getServicesCategory);
-router.post('/', validateJWT_1.validateJWT, servicesCategory_1.createCategory);
-//# sourceMappingURL=services_category.js.map
