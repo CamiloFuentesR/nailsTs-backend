c661071886d27a6285d3a645b90464ce79dc230a
Kmikmilo <lokomilo1@gmail.com>
2024-07-10 11:16:56 -0400

    prod

--- db/conection.ts
+++ db/conection.ts
@@ -1,29 +1,52 @@
-import { Sequelize, Dialect } from 'sequelize';
+// import { Sequelize, Dialect } from 'sequelize';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// const dbDatabase = process.env.POSTGRES_DATABASE || 'nails';
+// const dbUser = process.env.POSTGRES_USER || 'postgres';
+// const dbPassword = process.env.POSTGRES_PASSWORD;
+// const dbHost = process.env.POSTGRES_HOST;
+// const dbPort = parseInt(process.env.DB_PORT || '5432', 10);
+// const dbDialect = process.env.DB_DIALECT as Dialect;
+
+// const db = new Sequelize(dbDatabase, dbUser, dbPassword, {
+//   host: dbHost,
+//   port: dbPort,
+//   dialect: dbDialect,
+//   define: {
+//     timestamps: true,
+//     underscored: true,
+//     // freezeTableName: true,
+//   },
+//   timezone: 'America/Santiago',
+//   dialectOptions: {
+//     useUTC: false, // Para que no use UTC
+//     timezone: 'America/Santiago',
+//   },
+//   // logging: false
+// });
+
+// export default db;
+import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
-const dbDatabase = process.env.POSTGRES_DATABASE || 'nails';
-const dbUser = process.env.POSTGRES_USER || 'postgres';
-const dbPassword = process.env.POSTGRES_PASSWORD;
-const dbHost = process.env.POSTGRES_HOST;
-const dbPort = parseInt(process.env.DB_PORT || '5432', 10);
-const dbDialect = process.env.DB_DIALECT as Dialect;
-
-const db = new Sequelize(dbDatabase, dbUser, dbPassword, {
-  host: dbHost,
-  port: dbPort,
-  dialect: dbDialect,
-  define: {
-    timestamps: true,
-    underscored: true,
-    // freezeTableName: true,
+const db = new Sequelize(
+  process.env.POSTGRES_DATABASE!,
+  process.env.POSTGRES_USER!,
+  process.env.POSTGRES_PASSWORD!,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres', // Especifica el dialecto de PostgreSQL
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   },
-  timezone: 'America/Santiago',
-  dialectOptions: {
-    useUTC: false, // Para que no use UTC
-    timezone: 'America/Santiago',
-  },
-  // logging: false
-});
+);
 
 export default db;
--- dist/db/conection.js
+++ dist/db/conection.js
@@ -1,32 +1,47 @@
 "use strict";
+// import { Sequelize, Dialect } from 'sequelize';
+// import dotenv from 'dotenv';
+// dotenv.config();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// const dbDatabase = process.env.POSTGRES_DATABASE || 'nails';
+// const dbUser = process.env.POSTGRES_USER || 'postgres';
+// const dbPassword = process.env.POSTGRES_PASSWORD;
+// const dbHost = process.env.POSTGRES_HOST;
+// const dbPort = parseInt(process.env.DB_PORT || '5432', 10);
+// const dbDialect = process.env.DB_DIALECT as Dialect;
+// const db = new Sequelize(dbDatabase, dbUser, dbPassword, {
+//   host: dbHost,
+//   port: dbPort,
+//   dialect: dbDialect,
+//   define: {
+//     timestamps: true,
+//     underscored: true,
+//     // freezeTableName: true,
+//   },
+//   timezone: 'America/Santiago',
+//   dialectOptions: {
+//     useUTC: false, // Para que no use UTC
+//     timezone: 'America/Santiago',
+//   },
+//   // logging: false
+// });
+// export default db;
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const dbDatabase = process.env.POSTGRES_DATABASE || 'nails';
-const dbUser = process.env.POSTGRES_USER || 'postgres';
-const dbPassword = process.env.POSTGRES_PASSWORD;
-const dbHost = process.env.POSTGRES_HOST;
-const dbPort = parseInt(process.env.DB_PORT || '5432', 10);
-const dbDialect = process.env.DB_DIALECT;
-const db = new sequelize_1.Sequelize(dbDatabase, dbUser, dbPassword, {
-    host: dbHost,
-    port: dbPort,
-    dialect: dbDialect,
-    define: {
-        timestamps: true,
-        underscored: true,
-        // freezeTableName: true,
+const db = new sequelize_1.Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres', // Especifica el dialecto de PostgreSQL
+    logging: false,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
     },
-    timezone: 'America/Santiago',
-    dialectOptions: {
-        useUTC: false, // Para que no use UTC
-        timezone: 'America/Santiago',
-    },
-    // logging: false
 });
 exports.default = db;
 //# sourceMappingURL=conection.js.map
